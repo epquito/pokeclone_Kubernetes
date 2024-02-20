b2f395016b89c2f04c3b0e8ba91b823ff2756bc1
Stephan Lamoureux <sa.lamoureux@gmail.com>
2024-02-20 15:29:29 -0500

    removed app code, added rubric

--- back_end/pokemon_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PokemonAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pokemon_app'
--- back_end/team_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TeamManager
-
-urlpatterns = [
-    path('manager/', TeamManager.as_view(), name='get_user_teams_api'),
-    path('<int:team_id>/', TeamManager.as_view(), name='pick_unpick_pokemon'),
-]
