51bf4068521b2308151b5594bcc3c9e83b92dd95
Stephan Lamoureux <sa.lamoureux@gmail.com>
2024-02-24 14:27:43 -0500

    updated env vars to use vite's env import

--- front_end/src/components/utilities.jsx
+++ front_end/src/components/utilities.jsx
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-// Use the environment variable, or default to a placeholder if not set
-const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend-service.pokeclone.svc.cluster.local:8000";
+// Use the environment variable with Vite's import.meta.env, prefixed with VITE_
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://backend-service.pokeclone.svc.cluster.local:8000";
 
 // Create axios instances for different parts of your API using the base URL
 export const userApi = axios.create({
-  baseURL: `${REACT_APP_API_BASE_URL}/api/v1/user/`,
+  baseURL: `${API_BASE_URL}/api/v1/user/`,
 });
 
 export const wildApi = axios.create({
-  baseURL: `${REACT_APP_API_BASE_URL}/api/v1/pokemon/wild/`,
+  baseURL: `${API_BASE_URL}/api/v1/pokemon/wild/`,
 });
 
 export const pokeApi = axios.create({
-  baseURL: `${REACT_APP_API_BASE_URL}/api/v1/pokemon/`,
+  baseURL: `${API_BASE_URL}/api/v1/pokemon/`,
 });
 
 export const teamApi = axios.create({
-  baseURL: `${REACT_APP_API_BASE_URL}/api/v1/team/`,
+  baseURL: `${API_BASE_URL}/api/v1/team/`,
 });
 
 export const pokedexApi = axios.create({
-  baseURL: `${REACT_APP_API_BASE_URL}/api/v1/pokemon/pokedex/`,
+  baseURL: `${API_BASE_URL}/api/v1/pokemon/pokedex/`,
 });
